16809f093f87fa038d61de4a04714cad962656d1
Juan Pablo Forero <juandafloo55@gmail.com>
2023-03-21 08:56:33 -0500

    innit project

--- DaysWellness/app/src/main/java/com/example/dayswellness/MainActivity.kt
+++ DaysWellness/app/src/main/java/com/example/dayswellness/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.dayswellness.data.Days
 import com.example.dayswellness.ui.theme.DaysWellnessTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,8 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
-
+                    WellnessApp()
                 }
             }
         }
@@ -31,7 +34,19 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@Composable
+fun WellnessApp(){
 
+}
+
+@Composable
+fun CardApp(numberTip: Int, days: Days, modifier: Modifier = Modifier){
+    Card(modifier = Modifier.padding(8.dp), elevation = 4.dp) {
+        Text("Tip: $numberTip",
+        style = MaterialTheme.typography.h2, color = MaterialTheme.colors.secondary)
+    }
+
+}
 
 
 
