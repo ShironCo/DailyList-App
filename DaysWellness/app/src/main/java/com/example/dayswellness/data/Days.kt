cc74cc34152b9bfc942d3870af7f978398886a62
Juan Pablo Forero <juandafloo55@gmail.com>
2023-03-21 08:46:47 -0500

    innit project

--- DaysWellness/app/src/main/java/com/example/dayswellness/data/Days.kt
+++ DaysWellness/app/src/main/java/com/example/dayswellness/data/Days.kt
@@ -12,43 +12,43 @@ object DaysRepository{
     val days = listOf(
         Days(
             descripcionDay = "Despierta temprano para comenzar el día con energía y motivación.",
-            imgRes = R.drawable
+            imgRes = R.drawable.imag1
         ),
         Days(
             descripcionDay = "Haz estiramientos o yoga al despertar para activar tu cuerpo.",
-            imgRes = R.drawable
+            imgRes = R.drawable.imag2
         ),
         Days(
             descripcionDay = "Dedica unos minutos a meditar para reducir el estrés y mejorar tu salud mental.",
-            imgRes = R.drawable
+            imgRes = R.drawable.imag3
         ),
         Days(
             descripcionDay = "Planifica tus tareas y prioridades para aumentar tu productividad.",
-            imgRes = R.drawable
+            imgRes = R.drawable.imag4
         ),
         Days(
             descripcionDay = "Toma descansos regulares durante el día para evitar la fatiga mental y física.",
-            imgRes = R.drawable
+            imgRes = R.drawable.imag5
         ),
         Days(
             descripcionDay = "Aprende algo nuevo cada día para crecer personalmente y profesionalmente.",
-            imgRes = R.drawable
+            imgRes = R.drawable.imag6
         ),
         Days(
             descripcionDay = "Haz una lista de metas a corto y largo plazo para motivarte y alcanzar tus objetivos.",
-            imgRes = R.drawable
+            imgRes = R.drawable.imag7
         ),
         Days(
             descripcionDay = "Mantén una rutina de sueño consistente para mejorar tu salud y bienestar.",
-            imgRes = R.drawable
+            imgRes = R.drawable.imag8
         ),
         Days(
             descripcionDay = "Busca maneras de reducir tu impacto en el medio ambiente y ser más sostenible.",
-            imgRes = R.drawable
+            imgRes = R.drawable.imag9
         ),
         Days(
             descripcionDay = "Dedica tiempo a la reflexión y la introspección para entender tus emociones y pensamientos.",
-            imgRes = R.drawable
+            imgRes = R.drawable.imag10
         )
     )
 }
